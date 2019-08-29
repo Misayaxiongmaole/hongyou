ca47158fab905b2a3b9cadda52e4fc9d25610554
熊猫 <mac@macs-imac.local>
2019-08-29 14:36:49 +0800

    创建了一个person类

---
+++ text/text/PersonViewController.m
@@ -0,0 +1,32 @@
+//
+//  PersonViewController.m
+//  text
+//
+//  Created by mac on 2019/8/29.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import "PersonViewController.h"
+
+@interface PersonViewController ()
+
+@end
+
+@implementation PersonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"hello world");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
